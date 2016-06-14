5381aad43023a53ad54ee71eb3babb9bb345dd6c
wdy0401 <wdy0401@gmail.com>
2016-06-14 23:56:21 +0800

    clean main.cpp

--- main.cpp
+++ main.cpp
@@ -10,37 +10,21 @@ int main(int argc, char *argv[])
     Py_Initialize();
     pModule=PyImport_ImportModule("wadd");
 
-//    cerr<<"##4"<<endl;
-//    pValue=PyObject_CallMethod(pModule,"twadd","ii",10,20);
-//    cerr<<"##5"<<endl;
-//    cerr<<PyLong_AsLong(pValue)<<endl;
-
     pFunc=PyObject_GetAttrString(pModule, "twadd");
-    pValue=PyObject_CallFunction(pFunc,"ii",1000,1000);
-    pValue=PyObject_CallFunction(pFunc,"ii",10000,100);
     pValue=PyObject_CallFunction(pFunc,"ii",1000000);
     pValue=PyObject_CallFunction(pFunc,"ii",1000000);
     pValue=PyObject_CallFunction(pFunc,"ii",1000000);
     pValue=PyObject_CallFunction(pFunc,"ii",1000000);
     pValue=PyObject_CallFunction(pFunc,"ii",1000000);
+
+    pValue=PyObject_CallMethod(pModule,"twadd","ii",1000000000,20);
+    pValue=PyObject_CallMethod(pModule,"twadd","ii",1000000000,20);
+    pValue=PyObject_CallMethod(pModule,"twadd","ii",1000000000,20);
+    pValue=PyObject_CallMethod(pModule,"twadd","ii",1000000000,20);
+    pValue=PyObject_CallMethod(pModule,"twadd","ii",1000000000,20);
+    pValue=PyObject_CallMethod(pModule,"twadd","ii",1000000000,20);
 
     Py_Finalize();
 
     return a.exec();
 }
-//    pModule=PyImport_ImportModule("wadd");
-//    cerr<<"##4"<<endl;
-//    pValue=PyObject_CallMethod(pModule,"twadd","ii",10,20);
-//    cerr<<"##5"<<endl;
-//    cerr<<PyLong_AsLong(pValue)<<endl;
-
-//    cerr<<"##6"<<endl;
-//    pFunc=PyObject_GetAttrString(pModule, "twadd");
-//    cerr<<"##7"<<endl;
-//    pValue=PyObject_CallFunction(pFunc,"ii",1000);
-//    cerr<<"##8"<<endl;
-//    cout<<PyLong_AsLong(pValue)<<endl;
-//    cerr<<"##9"<<endl;
-//    pValue=PyObject_CallFunction(pFunc,"ii",1000,10);
-//    cout<<PyLong_AsLong(pValue)<<endl;
-//    pValue=PyObject_CallFunction(pFunc,"ii",1000);
