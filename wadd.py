d0bafb2dfb6091b17a89b5f920fcd6afd974df11
wdy0401 <wdy0401@gmail.com>
2016-07-07 14:43:24 +0800

    add another fun

--- wadd.py
+++ wadd.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
-c=0
+c=1
+c=c*2
+print('init')
 def twadd(a,b):
 	global c
 	c=c+a+b	
-	print("###")
+	print("###add")
+	print(a)
+	print(b)
+	print(c)
+	print("###end\n")
+	return c
+
+def twmul(a,b):
+	global c
+	c=2*(c+a+b)	
+	print("###mul")
 	print(a)
 	print(b)
 	print(c)
 	print("###\n")
 	return c
+
