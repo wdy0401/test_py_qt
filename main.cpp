cea3c091b2cd98bef48f776e9ef5bd4089e0011b
wdy0401 <wdy0401@gmail.com>
2016-08-02 16:25:55 +0800

    终于和正常写python一样调用python脚本了

--- main.cpp
+++ main.cpp
@@ -2,24 +2,36 @@
 #include<iostream>
 using namespace std;
 
+void init_py();
+
 int main()
 {
+    init_py();
+
     PyObject *pModule, *pFunc, *pValue;
-//    wchar_t  a[100]=L"C:/Anaconda3/Lib";
-//    Py_SetPythonHome(a);
-//    这段不管用
-    Py_Initialize();
-
     pModule=PyImport_ImportModule("wadd");
-
     pFunc=PyObject_GetAttrString(pModule, "twadd");
-
     pValue=PyObject_CallFunction(pFunc,"ii",10,100);
-
     pValue=PyObject_CallMethod(pModule,"twadd","ii",1,1000);
-
     pValue=PyObject_CallMethod(pModule,"twmul","ii",1,1000);
-
     Py_Finalize();
     return 0;
+}
+
+void init_py()
+{
+    Py_SetPythonHome((wchar_t *)L"C:/Anaconda3");
+     Py_Initialize();
+     PyRun_SimpleString("import sys");
+     PyRun_SimpleString("sys.path.append('C:\\Anaconda3\\python35.zip')");
+     PyRun_SimpleString("sys.path.append('C:\\Anaconda3\\DLLs')");
+     PyRun_SimpleString("sys.path.append('C:\\Anaconda3\\lib')");
+     PyRun_SimpleString("sys.path.append('C:\\Anaconda3')");
+     PyRun_SimpleString("sys.path.append('C:\\Anaconda3\\lib\\site-packages')");
+     PyRun_SimpleString("sys.path.append('C:\\Anaconda3\\lib\\site-packages\\Sphinx-1.4.1-py3.5.egg')");
+     PyRun_SimpleString("sys.path.append('C:\\Anaconda3\\lib\\site-packages\\win32')");
+     PyRun_SimpleString("sys.path.append('C:\\Anaconda3\\lib\\site-packages\\win32\\lib')");
+     PyRun_SimpleString("sys.path.append('C:\\Anaconda3\\lib\\site-packages\\Pythonwin')");
+     PyRun_SimpleString("sys.path.append('C:\\Anaconda3\\lib\\site-packages\\setuptools-23.0.0-py3.5.egg')");
+     PyRun_SimpleString("sys.path.append('C:/Wind/Wind.NET.Client/WindNET/bin')");
 }
